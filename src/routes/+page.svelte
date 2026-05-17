b1d0211b040698362f5a5daf61e506ff6b8cc00d
Shahin <shahinexy@gmail.com>
2026-05-17 16:06:09 +0600

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+const src = "hello"
+</script>
+
+<h1>Welcome to SvelteKit, {src}</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<a href="/about" >About Page</a>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+const src = "hello"
+</script>
+
+<h1 class="text-red-600 font-bold">{src} <span class="text-sm text-black">this is about page</span></h1>
+
+<a href="/" >Home Page</a>
